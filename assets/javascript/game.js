19d1ebd3d3cdc8815859bf0d5941087e94c642ca
cjstanzhu <cjstanzhu@gmail.com>
2019-06-13 01:31:37 -0400

    input game.js code

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    
+    var winCount = 0;
+    var loseCount = 0;
+    var targetNumber = 0;
+    var totalScore = 0;
+    var crystalValues = [];
+
+    function restartGame() {
+        targetNumber = Math.floor(Math.random() * 102) + 19;
+        totalScore = 0;
+        crystalValues = [];
+
+        for (var i = 0; i < 4; i++) {
+            var value = Math.floor(Math.random() * 12) + 1;
+            crystalValues.push(value);
+        };
+
+        $("#red-crystal").attr("data-crystalValue", crystalValues[0]);
+        $("#blue-crystal").attr("data-crystalValue", crystalValues[1]);
+        $("#yellow-crystal").attr("data-crystalValue", crystalValues[2]);
+        $("#green-crystal").attr("data-crystalValue", crystalValues[3]);
+
+        $("#target-number").text("Target number: " + targetNumber);
+        $("#total-score").text("Your total score: " + totalScore);
+    };
+
+    restartGame();
+
+    $(".crystal-image").on("click", function() {
+        var value = $(this).attr("data-crystalValue");
+        value = parseInt(value);
+
+        console.log(value);
+
+        totalScore += value;
+
+        if (totalScore === targetNumber) {
+            alert("You won!");
+            winCount++;
+            $("#win-count").text("Wins: " + winCount);
+            restartGame();
+        } else if (totalScore > targetNumber) {
+            alert("You lost!");
+            loseCount++;
+            $("#lose-count").text("Losses: " + loseCount);
+            restartGame();
+        } else {
+            $("#total-score").text("Your total score: " + totalScore);
+        };
+    });
+});
+
+
